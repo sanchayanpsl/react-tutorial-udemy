2b24a35ba67325d6e073bddce36346c181dc3686
sanchayan-das_psl <sanchayan_das@persistent.com>
2023-07-09 19:16:37 +0530

    code update for book list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from "react"
 import ReactDom from 'react-dom/client'
-
-const Gretting = () =>{
-    return <h2>My first Componnet</h2>
+import './index.css'
+const BookList = () => {
+    return <section className="booklist">
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+    </section>
 
 }
 
 
+const Book = () => {
+    return (
+        <article className="book">
 
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    )
+}
+
+const Image = () => <img alt="LEARNING AMAZON WEB SERVICES" src="https://m.media-amazon.com/images/I/51LpEQxoa1L._SY445_SX342_.jpg" />
+const Title = () => {
+    return <h2>LEARNING AMAZON WEB SERVICES</h2>
+}
+const Author = () => {
+    return <h4>Mark Wilkins</h4>
+}
 const root = ReactDom.createRoot(document.getElementById('root'))
 
-root.render(<Gretting/>)
+root.render(<BookList />)
